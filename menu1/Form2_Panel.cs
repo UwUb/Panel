dfefcb86a9acd8e35886929cf34269f65d8bf802
MEGAS <MEGAS@s03>
2023-05-29 21:14:02 +0800

    加入專案檔案。

---
+++ menu1/Form2_Panel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace menu1
+{
+    public partial class Form2_Panel : Form
+    {
+        public Form2_Panel()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
